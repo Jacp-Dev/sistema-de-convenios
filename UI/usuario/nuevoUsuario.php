4d5c37f19d77274dd52aee856206d83675513ea8
Jacp-Dev <144181159+Jacp-Dev@users.noreply.github.com>
2023-09-16 23:17:50 -0500

    Update nuevoUsuario.php

--- UI/usuario/nuevoUsuario.php
+++ UI/usuario/nuevoUsuario.php
@@ -16,6 +16,8 @@ error_reporting(0);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+     <link rel="icon" href="../../assets/img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="../../assets/img/favicon.ico" type="image/x-icon">
     <title>Registrar Usuario</title>
 </head>
 
