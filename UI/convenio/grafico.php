ab2c4b7082c12bb19ec51dc9d1e63993ee0e173e
Jacp-Dev <144181159+Jacp-Dev@users.noreply.github.com>
2023-09-16 23:13:54 -0500

    Update grafico.php

--- UI/convenio/grafico.php
+++ UI/convenio/grafico.php
@@ -16,6 +16,8 @@ error_reporting(0);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+   <link rel="icon" href="../../assets/img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="../../assets/img/favicon.ico" type="image/x-icon">
 
   <title>Gestion de Convenios</title>
 
