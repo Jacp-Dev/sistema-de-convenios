52f7fecb68e31a8cf27744cf6d25e51b55a1c81d
Jacp-Dev <144181159+Jacp-Dev@users.noreply.github.com>
2023-09-16 23:13:38 -0500

    Update detalleConvenio.php

--- UI/convenio/detalleConvenio.php
+++ UI/convenio/detalleConvenio.php
@@ -15,6 +15,8 @@ error_reporting(0);
   <link rel="stylesheet" href="../../assets/css/convenio.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+ <link rel="icon" href="../../assets/img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="../../assets/img/favicon.ico" type="image/x-icon">
   <title>Detalle convenio</title>
 </head>
 
